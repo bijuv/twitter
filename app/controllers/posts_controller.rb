580b8108a4bca20eddd72a7b791ac43a699c98f4
Your Name <bijuvidyadharan@yahoo.in>
2011-12-05 18:21:53 +0530

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   #skip_before_filter :authorize
 def index  
+    @users= User.all
     @user= current_user
     @posts = Post.all(:order => "created_at DESC")  
     respond_to do |format|  
@@ -12,9 +13,13 @@ def index
     @post = Post.create(:message => params[:message],:by => current_user)  
     respond_to do |format|  
       if @post.save  
-        redirect_to :back # solve the issue
-      else  
+        format.js  { render :json => @post, :status => :created }
+        format.html{ redirect_to("/",:notice => 'Posted')}
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else 
+        format.js  { render :json => @post.errors, :status => :unprocessable_entity } 
         redirect_to :back #solve the issue
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
       end  
     end  
   end  
